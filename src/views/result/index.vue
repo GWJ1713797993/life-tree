4ba93c0ece7b2e3773c5aa17d4ed04edd929c87f
高炜健 <a1713797993@163.com>
2020-07-24 14:42:41 +0800

    feat:增加保存图片显示二维码

--- src/views/result/index.vue
+++ src/views/result/index.vue
@@ -8,7 +8,13 @@
         <p class="page__name-p">你的专属<span class="page__name_text">本命树</span></p>
       </div>
       <img class="page__img" :src="url" alt="">
-      <!-- <img class="page__code page__img" v-show="false" src="@img/flower/code.png" alt=""> -->
+      <img class="page__code page__img" src="@img/flower/code.png" alt="">
+    </div>
+    <div class="tip-box">
+      <img class="tip-box__img" src="@img/flower/tip.png" alt="">
+      <div class="tip-box__btn" @click="toHappen">
+        发现颐安
+      </div>
     </div>
   </div>
 </template>
@@ -16,12 +22,12 @@
 <script>
 import { mapState } from 'vuex'
 import html2canvas from 'html2canvas'
-// import VConsole from 'vconsole'
+import VConsole from 'vconsole'
 export default {
   data() {
     return {
       src: '',
-      isShow: true
+      isShow: false
     }
   },
   computed: {
@@ -34,12 +40,12 @@ export default {
     }
   },
   created() {
-    // console.log(this.useName, this.time)
+    // console.log(this.useName, this.time, 'xxxx')
     window.addEventListener('load', e => this.refreshFun(e))
   },
   mounted() {
-    // const vconsole = new VConsole()
-    // console.log(vconsole)
+    const vconsole = new VConsole()
+    console.log(vconsole)
     html2canvas(document.querySelector('#capture')).then(canvas => {
       // document.body.appendChild(canvas)
       // 导出为base64
@@ -50,6 +56,11 @@ export default {
     refreshFun() {
       this.$router.replace({
         name: 'Home'
+      })
+    },
+    toHappen() {
+      this.$router.replace({
+        name: 'Happen'
       })
     }
   },
@@ -91,6 +102,29 @@ export default {
     top: 0;
     left: 0;
     z-index: 9999;
+    opacity: 0;
+  }
+}
+.tip-box{
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 10.2rem;
+  // z-index: 1;
+  &__img{
+    width: 100%;
+  }
+  &__btn{
+    width: 2.34rem;
+    line-height: 0.6rem;
+    color: #fefefe;
+    font-size: 0.32rem;
+    text-align: center;
+    background: url('~@img/flower/btn-bg.png') no-repeat;
+    background-size: 100% 100%;
+    position: absolute;
+    top: 50%;
+    right: 0;
   }
 }
 </style>
